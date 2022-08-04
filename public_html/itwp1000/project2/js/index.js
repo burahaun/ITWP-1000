f33bd2538983f1bf92ac8f1e1ff7a41e8b43d2a2
burahaun <burahaunuddin@gmail.com>
2022-08-04 17:02:49 -0400

    first commit

---
+++ public_html/itwp1000/project2/js/index.js
@@ -0,0 +1,3 @@
+document.write("<div style='text-align:center;'>");
+document.write("<a href='default.html'>Homepage</a> | <a href='ai.html'>Artificial Intelligence</a> | <a href='software.html'>Software Engineering</a> | <a href='ml.html'>Machine Learning</a");
+document.write("</div>")
---
+++ public_html/itwp1000/project4/assets/js/index.js
@@ -0,0 +1,3 @@
+document.write("<div style='text-align:center;'>");
+document.write("<a href='index.html'>Homepage</a> | <a href='twocolumn1.html'>Rules & Categories</a> | <a href='twocolumn2.html'>Submit</a> | <a href='onecolumn.html'>Vote</a");
+document.write("</div>")
